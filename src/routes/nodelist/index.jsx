1644c6e5e044d8bc4e91a8bba69a5772040d84d2
Steve <admin@cryptostat.org>
2019-11-12 13:32:27 +1100

    add test data

--- src/routes/nodelist/index.jsx
+++ src/routes/nodelist/index.jsx
@@ -6,17 +6,31 @@ import Pager from "../../components/Pager";
 import voteOutImage from "../../assets/img/vote_out.svg";
 
 export default function NodeList() {
-  const [data, setData] = useState({ rows: [] });
+  const [data, setData] = useState({ blockProducers: [], peers: [] });
 
   // fetch real data here
   useEffect(() => {
     async function fetchData() {
-      // const result = await axios("https://jsonplaceholder.typicode.com/posts/");
-      // console.log("result", result);
-      setData({ rows: [1, 1, 1, 1, 1, 1, 1, 1] });
+      const result = await axios("http://localhost:4002/api/state");
+      let blockProducers = []
+      let peers = []
+
+      for(let i = 0; i < result.data.data.nodes.length; i++) {
+        if(result.data.data.nodes[i].role === "MasterNode") {
+          blockProducers.push(result.data.data.nodes[i])
+        }
+        if(result.data.data.nodes[i].role === "PeerNode") {
+          peers.push(result.data.data.nodes[i])
+        }
+
+      }
+      console.log(blockProducers)
+      console.log(peers)
+      setData({ blockProducers, peers});
     }
     fetchData();
   }, []);
+
 
   return (
     <Layout tabIndex={1}>
@@ -25,22 +39,22 @@ export default function NodeList() {
           <div className="nodelist__header">
             <div className="node_info">
               <span className="node_text">Block Producers / Validators</span>
-              <span className="node_number">10</span>
+              <span className="node_number">{data.blockProducers.length}</span>
             </div>
             <div className="node_info">
               <span className="node_text">Peers</span>
-              <span className="node_number">7</span>
+              <span className="node_number">{data.peers.length}</span>
             </div>
           </div>
 
           <div className="divide-line" />
           <div className="nodelist__cards">
             <div className="nodelist__cards__column">
-              {data.rows.map((item, index) => (
+              {data.blockProducers.map((item, index) => (
                 <div className="node_card" key={`$nodecard_${index}`}>
                   <div className="node_card__left">
-                    <p className="p1">flinders.ciredwc22sf32scxzcdvet3234</p>
-                    <p>0xfsdfigtigjf3432igv45uovigk3ig5</p>
+                    <p className="p1">{item.name}</p>
+                    <p>{item.publicKey}</p>
                   </div>
                   <div className="node_card__right">
                     <button className="vote-out">
@@ -52,16 +66,16 @@ export default function NodeList() {
               ))}
             </div>
             <div className="nodelist__cards__column">
-              {data.rows.map((item, index) => (
+              {data.peers.map((item, index) => (
                 <div className="node_card" key={`$nodecard_${index}`}>
                   <div className="node_card__left">
-                    <p className="p1">flinders.ciredwc22sf32scxzcdvet3234</p>
-                    <p>0xfsdfigtigjf3432igv45uovigk3ig5</p>
+                    <p className="p1">{item.name}</p>
+                    <p>{item.publicKey}</p>
                   </div>
                   <div className="node_card__right">
                     <button className="vote-out">
                       <img src={voteOutImage} alt="vote out" />
-                      &nbsp;&nbsp;Vote out
+                      &nbsp;&nbsp;Vote in
                     </button>
                   </div>
                 </div>
