3066502d2a3c48f470295006e3acfa9b868bf5de
Steve <admin@cryptostat.org>
2019-12-23 15:50:43 +1100

    change default page to nodelist

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,5 +1,5 @@
 export const defaultMenuType = 'menu-hidden'; // 'menu-default', 'menu-sub-hidden', 'menu-hidden';
-export const defaultStartPath = '/governance';
+export const defaultStartPath = '/governance/nodes';
 export const subHiddenBreakpoint = 1440;
 export const menuHiddenBreakpoint = 768;
 export const defaultLocale = 'en';
@@ -11,6 +11,6 @@ export const searchPath = "/app/search"
 export const servicePath = "https://api.coloredstrategies.com"
 
 export const baseURL = `${document.location.origin}/governancesvc`
-// url for test 
+// url for test
 // export const baseURL = `http://toorak05.ledgerium.io/governancesvc`
 // 'https://flinders01.ledgerium.io/governancesvc'
